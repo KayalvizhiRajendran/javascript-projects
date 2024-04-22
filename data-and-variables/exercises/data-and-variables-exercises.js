1047da32b3fab8a86bfcd7dea4f18f38d7a253c6
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-04-21 22:43:08 -0500

    Calculation Space Mission Excercise - for Mars and Moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,26 @@
 // Declare and assign the variables below
-
+let spaceShuttleName = 'Determination'
+let spaceShuttleSpeed = 17500
+let distanceToMars = 225000000
+let distanceToMoon = 384400
+let milesPerKm = 0.621
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof(spaceShuttleName))
+console.log(typeof(spaceShuttleSpeed))
+console.log(typeof(distanceToMars))
+console.log(typeof(distanceToMoon))
+console.log(typeof(milesPerKm))
 // Calculate a space mission below
+let milesToMars = distanceToMars * milesPerKm
+let hoursToMars = milesToMars/spaceShuttleSpeed
+let daysToMars = hoursToMars /24
 
 // Print the results of the space mission calculations below
-
+console.log("%s will take %f days to reach Mars.",spaceShuttleName, daysToMars)
 // Calculate a trip to the moon below
+let milesToMoon = distanceToMoon * milesPerKm
+let hoursToMoon = milesToMoon/spaceShuttleSpeed
+let daysToMoon = hoursToMoon /24
 
 // Print the results of the trip to the moon below
+console.log("%s will take %f days to reach Moon.",spaceShuttleName, daysToMoon)
--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,6 +1,9 @@
    // This demo shows off comments!
+/* Examples for Comment 
+   in Javascript       
+   Class Name: Comments.js */
 
-   // console.log("This does not print.");
+   console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
@@ -10,3 +13,5 @@
    comments. */
 
    console.log("Comments make your code more readable by others.");
+  
+   // End of Comments.Js
