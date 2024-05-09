30098f3e6971593a75ab511c2cae6bfe8a1f590f
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-05-09 14:23:27 -0500

    String and Array - Excersise completed

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,38 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+
+myStr = 'Index';
+console.log(myStr[2] === 'n');
+console.log(myStr[4] === 'x');
+console.log(myStr[6] === ' ');
+console.log(myStr[0] === 'I');
+
+phrase = 'Code for fun';
+console.log(phrase[2]);
+
+cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;
+
+console.log(location.length);
+
+let pet = 'cat';
+console.log(pet +"s");
+pet += "s";
+console.log(pet);
+
+let language = "JavaScript";
+console.log(language.replace('J', 'Q'));
+console.log(language.slice(0,5));
+console.log(language);
+
+console.log( language.slice(1,6));
+
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -3,6 +3,34 @@
 let word = 'JavaScript';
 
 console.log(word.toUpperCase());
+
+let codes = [76, 97, 117, 110, 99, 104, 67, 111, 100, 101];
+
+let characters = String.fromCharCode(codes[0]) + String.fromCharCode(codes[1])
+                + String.fromCharCode(codes[2]) + String.fromCharCode(codes[3])
+                + String.fromCharCode(codes[4]) + String.fromCharCode(codes[5])
+                + String.fromCharCode(codes[6]) + String.fromCharCode(codes[7])
+                + String.fromCharCode(codes[8]) + String.fromCharCode(codes[9]);
+
+console.log(characters);
+
+console.log('Launch\nCode');
+console.log('Launch/nCode');
+console.log('Launch', 'Code');
+console.log('Launch\tCode');
+console.log('Launch/tCode');
+
+let pluralNoun = "structures";
+let name = "Tom";
+let verb = "code";
+let adjective = "beautiful";
+let color = "red";
+
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective}  syntax and ${pluralNoun} — that allows ${name} to ${verb} with strings.`);
+name = "Jack";
+let currentAge = 9;
+
+console.log(`Next year, ${name} will be ${currentAge + 1}.`);
 //Returns ``JAVASCRIPT``
 
 //What does ``word.slice(4).toUpperCase()`` return?
