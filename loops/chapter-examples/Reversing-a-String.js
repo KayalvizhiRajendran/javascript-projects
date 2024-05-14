bbb6c084c925dcef290623b34647d098e8eca371
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-05-14 14:48:49 -0500

    Loops - Chapter code tried and verified

--- loops/chapter-examples/Reversing-a-String.js
+++ loops/chapter-examples/Reversing-a-String.js
@@ -6,3 +6,9 @@ for (let i = 0; i < str.length; i++) {
 }
 
 console.log(reversed);
+
+let phrase = "LaunchCode's LC101";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
+let familyMemberNames = ["Neelam Sathish", "Kayalvizhi", "Neelaveni", "Neelesh"];
 
+for (let i = 0; i < familyMemberNames.length; i++) {
+    console.log(familyMemberNames[i]);
+}
 // write a for loop that prints each name on a different line
