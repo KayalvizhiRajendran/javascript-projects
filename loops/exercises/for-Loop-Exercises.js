7ebd272ef94c7af3ce7f87ca4b006e1fe5160821
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-05-14 14:47:21 -0500

    Loops - Excercise completed

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,9 +4,24 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+for (let index = 0; index < 21; index++) {
+  console.log(index);
+}
 
+for (let index = 3; index < 30; index = index+2) {
+  console.log(index);
+}
 
+for (let index = 12; index > -15; index--) {
+  console.log(index);
+}
 
+for (let index = 50 ; index > 19; index--) {
+  
+  if(index % 3 == 0){
+    console.log(index);
+  }
+}
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
@@ -14,11 +29,36 @@ Initialize two variables to hold the string “LaunchCode” and the array [1, 5
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+let name = "LaunchCode";
+let array = [1,5,'LC101','blue',42];
+let reversed = "";
 
+for (let index = 0; index < array.length; index++) {
+  console.log( array[index]);
+}
 
+for (let i = 0; i < name.length; i++) {
+  reversed = name[i] + reversed;
+}
 
+console.log(reversed);
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+  let otherArr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [], odds = [];
+
+for (let index = 0; index < otherArr.length; index++) {
+  const element = Number(otherArr[index]);
+  console.log(element);
+  if(element % 2 == 0){
+    evens.push(element);
+  }else{
+    odds.push(element);
+  }
+  
+}
+console.log(evens);
+console.log(odds);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,50 @@
+const input = require('readline-sync');
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
-
-
-
-
+let fuelLevel ;
+let noOfastronaut;
+let altitude;
+let aptFuelLevel = false;
+let aptNoofAtronauts = false;
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-
+  while(!aptFuelLevel){
+    fuelLevel = Number(input.question("Enter a fuel level..(Between 5000 - 30000) "));
+    if (fuelLevel > 5000 && fuelLevel < 30000){
+      aptFuelLevel = true;
+      break;
+    }
+    }
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
   
+while(!aptNoofAtronauts){
+  noOfastronaut = Number(input.question("Enter the number of Astranauts(0-7): "));
+  if(noOfastronaut>0 && noOfastronaut <=7){
+    aptNoofAtronauts = true;
+    break;
+  }
+}
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
+altitude =0;
+let minRequiredFuel = noOfastronaut*50;
+while(fuelLevel > minRequiredFuel){
+  altitude = altitude + 50;
+  fuelLevel = fuelLevel - 100;
+}
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+console.log(`The shuttle gained an altitude of ${altitude} km.`);
+
+if(altitude >=2000){
+  console.log("Orbit Achieved!");
+}else{
+  console.log("Failed to reach orbit.");
+}
