d61bf37beacb1faf3fd016bac12fa1f1637b1560
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-08-02 22:53:24 -0500

    Exercise - dom and events completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener("click", function (event) {
+        paragraph.innerHTML = "Houston, we have liftoff!"; 
+      });
+      
+      missionAbort.addEventListener("mouseover", function( event ) {
+        missionAbort.style.backgroundColor = "red";
+     });
+
+      missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
+     missionAbort.addEventListener("click", function(event){
+        let confirm = window.confirm("Are you sure you want to abort the mission?");
+        if(confirm === true){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+     });
     
 }
 
